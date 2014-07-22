26bf9c70235b1afc8f3d91f09eaf86524864eff8
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-07-22 16:40:21 +0200

    removed cp not needed for now

--- lib/generators/configatron/install/install_generator.rb
+++ lib/generators/configatron/install/install_generator.rb
@@ -2,7 +2,6 @@ require 'rails/generators/base'
 
 class Configatron
   class InstallGenerator < ::Rails::Generators::Base #:nodoc:
-
     desc 'Generates configatron files for the default Rails environments.'
 
     def self.source_root
@@ -15,11 +14,6 @@ class Configatron
 
     def copy_files
       template 'initializers/configatron.rb', 'config/initializers/configatron.rb'
-
-      %w{defaults development production test}.each do |env|
-        template "configatron/#{env}.rb", "config/configatron/#{env}.rb"
-      end
     end
-
   end
 end
